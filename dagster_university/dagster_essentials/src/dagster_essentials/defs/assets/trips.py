2a20b3f7508f1f592aadb438367a82be44996790
erguezjorge <erguezjorge@gmail.com>
2026-07-13 10:53:22 +0000

    lecciones 1 a 9

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -0,0 +1,106 @@
+import requests
+import dagster as dg
+
+from dagster_duckdb import DuckDBResource
+
+from dagster_essentials.defs.assets import constants
+from dagster_essentials.defs.partitions import monthly_partition
+
+
+
+@dg.asset(
+  partitions_def=monthly_partition
+)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
+  """
+  The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+  """
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  raw_trips = requests.get(
+    f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+  )
+
+  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+    output_file.write(raw_trips.content)
+
+
+@dg.asset(
+  deps=["taxi_trips_file"],
+  partitions_def=monthly_partition
+)
+def taxi_trips(context: dg.AssetExecutionContext,
+               database: DuckDBResource) -> None:
+  """
+  The raw taxi trips dataset, loaded into a DuckDB database
+  """
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );
+
+    delete from trips where partition_date = '{month_to_fetch}';
+
+    insert into trips
+    select
+      VendorID as vendor_id,
+      PULocationID as pickup_zone_id,
+      DOLocationID as dropoff_zone_id,
+      RatecodeID as rate_code_id,
+      payment_type as payment_type,
+      tpep_dropoff_datetime as dropoff_datetime,
+      tpep_pickup_datetime as pickup_datetime,
+      trip_distance as trip_distance,
+      passenger_count as passenger_count,
+      total_amount as total_amount,
+      {month_to_fetch} as partition_date
+    from
+      '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}'
+    ;
+  """
+
+  with database.get_connection() as conn:
+    conn.execute(query)
+
+
+@dg.asset
+def taxi_zones_file() -> None:
+  """
+  This asset will contain a unique identifier and name for each part of NYC as a distinct taxi zone
+  """
+  raw_taxi_zones = requests.get(
+    f"https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+  )
+
+  with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+    output_file.write(raw_taxi_zones.content)
+
+
+@dg.asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones(database: DuckDBResource) -> None:
+  """
+  The raw taxi zones csv, loaded into a DuckDB database
+  """
+  query = """
+    create or replace table zones as (
+    select
+      LocationID as zone_id,
+      zone,
+      borough,
+      the_geom as geometry
+    from
+      'data/raw/taxi_zones.csv'
+    );
+  """
+
+  with database.get_connection() as conn:
+    conn.execute(query)
